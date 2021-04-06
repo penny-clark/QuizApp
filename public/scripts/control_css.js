6b0450ed1b6edd84fa9a6529b39535d401f501e0
Minjae Jung <noahjung87@gmail.com>
2021-04-06 16:54:15 +0000

    maker-result page & control_css.js

---
+++ public/scripts/control_css.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+  $("#view_quiz").on( "click", function() {
+    $("#view_quiz_display").css("display","block");
+    $("#view_quiz").css("display","none");
+});
+
+});
