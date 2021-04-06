ea561a92a92021f7a6f9a4447f9df5363af992e1
Penny Clark <respect.cactus@gmail.com>
2021-04-06 23:12:20 +0000

    add preliminary code for inserting new quiz fields

--- database.js
+++ database.js
@@ -39,6 +39,21 @@ const getAttemptsResults = function(attempt_id) {
   })
 }
 
+//get single attempt score as a percentage
+const getAttemptScorePercentage = function(attempt_id) {
+  return pool.query(`
+  SELECT attempts.quiztaker_name as name, attempts.quiz_id as quiz_id, (count(answers.correct) / count(attempts_answers.*)) * 100 as average
+  FROM attempts_answers
+  JOIN answers ON answers.id = answer_id
+  JOIN attempts ON attempts.id = attempt_id
+  WHERE attempts.id = $1
+  GROUP BY attempts.id`, [attempt_id])
+  .then(res => {
+    const attemptScore = res.rows[0] || null;
+    return attemptScore;
+  })
+}
+
 //get quiz results for all takers of a single quiz
 const getAllResultsForQuiz = function(quiz_id) {
   return pool.query(`
@@ -67,5 +82,54 @@ const getQuizTotalAverage = function(quiz_id) {
   .then(res => {
     const quizAverage = res.rows[0] || null;
     return quizAverage;
+  })
+}
+
+//add a new quiz to the quizzes database
+const addQuiz = function(newquiz) {
+  return pool.query(`INSERT INTO quizzes (title, creator_name, publicly_listed, category)
+  VALUES ($1, $2, $3, $4, $5) RETURNING *;`, [newquiz.title, newquiz.creatorname, newquiz.public, newquiz.category])
+  .then((res) => {
+    const newquiz = res.rows[0];
+    console.log(newquiz); //console log for testing
+    return newquiz;
+  })
+}
+
+//add a new question to questions database
+//WHETHER THIS WORKS OR NOT WILL DEPEND ON WHETHER THE QUIZ ID CAN BE PASSED
+//IN A PROMISE CHAIN THIS WAY (or with adjusted syntax)
+//SAME GOES FOR THE addAnswer FUNCTIONS
+const addQuestion = function(newquestion) {
+  return pool.query(`INSERT INTO questions (quiz_id, question_content)
+  VALUES (${newquiz.id}, $1,) RETURNING *;`, [newquestion.content])
+  .then((res) => {
+    const newquestion = res.rows[0];
+    console.log(newquestion); //console log for testing
+    return newquestion;
+  })
+}
+
+//add a correct answer to answers database
+//We'll need to pass the question id to the other answers in the chain too
+//not sure if we can do this by returning the newquestion again in the .then
+//or by accessing the question_id field of the previously returned answer
+const addCorrectAnswer = function(correctanswer) {
+  return pool.query(`INSERT INTO answers (question_id, answer_content, correct)
+  VALUES (${newquestion.id}, $1, 'true') RETURNING *;`, [correctanswer.content])
+  .then((res) => {
+    const correctAnswer = res.rows[0];
+    console.log(correctAnswer); //console log for testing
+    return correctAnswer;
+  })
+}
+
+const addIncorrectAnswer = function(incorrectanswer) {
+  return pool.query(`INSERT INTO answers (question_id, answer_content)
+  VALUES (${newquestion.id}, $1) RETURNING *;`, [incorrectanswer.content])
+  .then((res) => {
+    const incorrectAnswer = res.rows[0];
+    console.log(incorrectAnswer); //console log for testing
+    return incorrectAnswer;
   })
 }
