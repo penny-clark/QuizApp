3ccff3b7a4ceddfb2fb016f3af66fe2646d74b56
Minjae Jung <noahjung87@gmail.com>
2021-04-06 18:40:53 +0000

    taker-result see detail BT behavior

--- public/scripts/control_css.js
+++ public/scripts/control_css.js
@@ -3,6 +3,13 @@ $( document ).ready(function() {
   $("#view_quiz").on( "click", function() {
     $("#view_quiz_display").css("display","block");
     $("#view_quiz").css("display","none");
-});
+  });
+
+  $("#result_detail").on( "click", function() {
+    $(".quiz_result").css("display","block");
+    $("#result_detail").css("display","none");
+  });
+
+
 
 });
