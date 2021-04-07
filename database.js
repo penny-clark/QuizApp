dbaf6d031548b6ce24b5a9453d870d2827268aaa
Penny Clark <respect.cactus@gmail.com>
2021-04-07 22:03:59 +0000

    added default null value to answers table and made a small change to start working on database functions

--- database.js
+++ database.js
@@ -85,7 +85,7 @@ const getQuizTotalAverage = function(quiz_id) {
   })
 }
 
-//add a new quiz to the quizzes database
+//THESE FOUR FUNCTIONS ARE FOR ADDING A NEW QUIZ
 const addQuiz = function(newquiz) {
   return pool.query(`INSERT INTO quizzes (title, creator_name, publicly_listed, category)
   VALUES ($1, $2, $3, $4, $5) RETURNING *;`, [newquiz.title, newquiz.creatorname, newquiz.public, newquiz.category])
