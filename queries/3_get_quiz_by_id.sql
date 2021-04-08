a858b8fb1979944a59b3d2af5b5fa31f5f004cbe
Penny Clark <respect.cactus@gmail.com>
2021-04-08 03:00:32 +0000

    update to query 3 to get info for maker results page. Formatting and exports on database.js

--- queries/3_get_quiz_by_id.sql
+++ queries/3_get_quiz_by_id.sql
@@ -1,11 +1,11 @@
 --query to select all of a quiz's fields based on the quiz id
-SELECT quizzes.title as quiz_name, questions.question_content as question,
+SELECT DISTINCT quizzes.title as quiz_name, questions.question_content as question,
 (SELECT answers.answer_content FROM answers
 JOIN questions on question_id = questions.id
-WHERE questions.quiz_id = 1 AND correct = 'true') as correctanswer,
+WHERE questions.quiz_id = 1 AND answers.correct = 'true') as correctanswer,
 (SELECT answers.answer_content FROM answers
 JOIN questions on question_id = questions.id
-WHERE questions.quiz_id = 1 AND correct IS NULL) as wronganswer
+WHERE questions.quiz_id = 1 AND answers.correct IS NULL) as wronganswer
 FROM quizzes
 JOIN questions ON quizzes.id = quiz_id
 JOIN answers ON questions.id = question_id
